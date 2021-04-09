93ad1195889fb83851a2d77c5bcd1e4b0818024b
Johan Söderlind Åström <johan.soderlind.astrom@gmail.com>
2021-04-10 00:13:57 +0200

    misc

--- demo1/Demo_Controller.cs
+++ demo1/Demo_Controller.cs
@@ -24,38 +24,81 @@ using Microsoft.AspNetCore.Hosting;
 
 namespace demo1
 {
-    
 
-    
-    public class Demo_Controller : Controller {
+	public class Layout
+	{
+		public string name { get; set; }
+		public int offset { get; set; }
+		public int size { get; set; }
+	}
 
-        public Demo_Controller()
-        {
-        }
+	public class Door
+	{
+		public string name { get; set; }
+		public List<Layout> layouts { get; set; }
+	}
 
+	public class Demo_Controller : Controller
+	{
 
-        //https://stackoverflow.com/questions/61104053/how-do-properly-stream-a-video-using-partial-content-ranges-from-azure-blob-stor
-        //https://blogs.visigo.com/chriscoulson/easy-handling-of-http-range-requests-in-asp-net/
-        //https://stackoverflow.com/questions/48711209/partial-content-in-net-core-mvc-for-video-audio-streaming/48712243
-        [HttpGet("/data/{**name}")]
-        public IActionResult GetFileDirect(string name)
-        {
-            string path = Path.Combine(Directory.GetCurrentDirectory(), "data", name);
-            Console.WriteLine(path);
-            var res = File(System.IO.File.OpenRead(path), "application/octet-stream");
-            res.EnableRangeProcessing = true;
-            return res;
-        } 
+		public Demo_Controller()
+		{
+		}
 
 
+		//https://stackoverflow.com/questions/61104053/how-do-properly-stream-a-video-using-partial-content-ranges-from-azure-blob-stor
+		//https://blogs.visigo.com/chriscoulson/easy-handling-of-http-range-requests-in-asp-net/
+		//https://stackoverflow.com/questions/48711209/partial-content-in-net-core-mvc-for-video-audio-streaming/48712243
+		[HttpGet("/data/{**name}")]
+		public IActionResult GetFileDirect(string name)
+		{
+			string path = Path.Combine(Directory.GetCurrentDirectory(), "data", name);
+			Console.WriteLine(path);
+			var res = File(System.IO.File.OpenRead(path), "application/octet-stream");
+			res.EnableRangeProcessing = true;
+			return res;
+		}
 
-        [HttpGet("/json")]
-        public async Task<IActionResult> json()
-        {
-            string s = JsonSerializer.Serialize<string>("Hello");
-            return Content(s,"application/json");
-        }
+		[HttpGet("/projects")]
+		public async Task<IActionResult> projects()
+		{
+			string path = Path.Combine(Directory.GetCurrentDirectory(), "data");
+			List<string> dirs = new List<string>(Directory.EnumerateDirectories(path));
+			for(var i = 0; i < dirs.Count; ++i)
+			{
+				dirs[i] = dirs[i].Substring(dirs[i].LastIndexOf(Path.DirectorySeparatorChar) + 1);
+			}
+			string s = JsonSerializer.Serialize<List<string>>(dirs);
+			return Content(s, "application/json");
+		}
+
+		[HttpGet("/doors")]
+		public async Task<IActionResult> doors()
+		{
+			List<Door> doors = new List<Door>();
+			string path = Path.Combine(Directory.GetCurrentDirectory(), "data");
+			List<string> names = new List<string>(Directory.EnumerateFiles(path, "*.json"));
+			for(var i = 0; i < names.Count; ++i)
+			{
+				Door door = new Door();
+				door.name = Path.GetFileNameWithoutExtension(names[i]);
+				string content = System.IO.File.ReadAllText(names[i]);
+				door.layouts = JsonSerializer.Deserialize<List<Layout>>(content);
+				doors.Add(door);
+				//Console.WriteLine(names[i]);
+			}
+			string s = JsonSerializer.Serialize<List<Door>>(doors);
+			return Content(s, "application/json");
+		}
 
 
-    }
+		[HttpGet("/json")]
+		public async Task<IActionResult> json()
+		{
+			string s = JsonSerializer.Serialize<string>("Hello");
+			return Content(s, "application/json");
+		}
+
+
+	}
 }
--- demo1/wwwroot/scripts.js
+++ demo1/wwwroot/scripts.js
@@ -1,7 +1,8 @@
 //http://www.davejennifer.com/computerjunk/javascript/tail-dash-f.html
 
 
-function swap32(val) {
+function swap32(val)
+{
 	return ((val & 0xFF) << 24)
 		| ((val & 0xFF00) << 8)
 		| ((val >> 8) & 0xFF00)
@@ -11,7 +12,7 @@ function swap32(val) {
 
 
 
-function load(filename, from, to, callback)
+function req_arraybuffer(filename, from, to, callback)
 {
 	var meter = document.getElementById('value1');
 	console.log("Loading file: " + filename);
@@ -20,26 +21,30 @@ function load(filename, from, to, callback)
 	//xhr.overrideMimeType("application/octet-stream");
 	xhr.open("GET", filename, true);
 	xhr.setRequestHeader('Range', 'bytes=' + from + '-' + to);
-	xhr.onprogress = (event) => {
+	xhr.onprogress = (event) =>
+	{
 		meter.value = (100 * event.total / event.loaded);
 		console.log("Downloaded ${event.loaded} of ${event.total} bytes");
 	}
-	xhr.onload = function (e) {
+	xhr.onload = function (e)
+	{
 		var buffer = xhr.response;
 		var f = new Float32Array(buffer, 0, buffer.byteLength / 4);
 		var n = f.length / 4;
 		var x = new Float32Array(n);
 		var y = new Float32Array(n);
 		var z = new Float32Array(n);
-		for (var i = 0; i < n; i++) {
+		for (var i = 0; i < n; i++)
+		{
 			x[i] = (f[i * 4 + 0]);
 			y[i] = (f[i * 4 + 1]);
 			z[i] = (f[i * 4 + 2]);
+			//w[i] = (f[i * 4 + 3]);Light intensity
 		}
-		console.log(f);
-		console.log(x);
-		console.log(y);
-		console.log(z);
+		//console.log(f);
+		//console.log(x);
+		//console.log(y);
+		//console.log(z);
 		callback(x, y, z);
 	};
 	xhr.onerror = function (e)
@@ -57,9 +62,9 @@ function load(filename, from, to, callback)
 
 
 
-function load_layout(filename, callback)
+function req_json(filename, callback)
 {
-	console.log("Loading file: " + filename);
+	console.log("Requesting json from: " + filename);
 	var xhr = new XMLHttpRequest();
 	xhr.open("GET", filename, true);
 	xhr.onload = function (e)
